a74ffd72b51bbc9e62ba3e4b6472f0f48f83c2c4
lihao <lihao3016@gmail.com>
2018-02-10 21:54:09 +0800

    hough lines

--- opencv/houghline/hough_lines.py
+++ opencv/houghline/hough_lines.py
@@ -1,30 +1,36 @@
-#!/usr/bin/python2
-#-*-coding:utf8-*-
+#!/usr/bin/python2.7
+#-*-cosding:utf8-*-
 import cv2
 import numpy as np
 def detect_lines(cnt):
-	img = cv2.imread('input.jpeg')
-	cnt = cv2.getTrackbarPos('cnt', 'image')
-	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-	edges = cv2.Canny(gray,50,200)
-	lines = cv2.HoughLines(edges,1,np.pi/180,cnt)
-	lines1 = lines[:,0,:]#提取为为二维
-	print lines1.shape
-	for rho,theta in lines1[:]: 
-		a = np.cos(theta)
-		b = np.sin(theta)
-		x0 = a*rho
-		y0 = b*rho
-		x1 = int(x0 + 1000*(-b))
-		y1 = int(y0 + 1000*(a))
-		x2 = int(x0 - 1000*(-b))
-		y2 = int(y0 - 1000*(a)) 
-		cv2.line(img,(x1,y1),(x2,y2),(0,255,0),1)
-	cv2.imshow("image", cv2.pyrDown(cv2.pyrDown(img)))
-img = cv2.imread('input.jpeg') 
+    img = cv2.imread('input.jpeg')
+    cnt = cv2.getTrackbarPos('cnt', 'image')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 50, 200)
+    lines = cv2.HoughLines(edges, 1, np.pi/180, cnt)
+    if type(lines) == type(None):
+        print "length of lines ==0"
+        return
+    lines = lines[:,0,:]
+    xMax, yMax = edges.shape
+    for rho, theta in lines:
+        print np.sin(theta)
+        if np.sin(theta)>0.5:
+            x0, x1 = 0, xMax
+            y0 = int((-x0*np.cos(theta) + rho)/np.sin(theta))
+            y1 = int((-x1*np.cos(theta) + rho)/np.sin(theta))
+        else:
+            y0, y1 = 0, yMax
+            x0 = int((-y0*np.sin(theta) + rho)/np.cos(theta))
+            x1 = int((-y1*np.sin(theta) + rho)/np.cos(theta))
+        cv2.line(img, (x0, y0), (x1, y1), (0, 255, 0), 1)
+    cv2.imshow('image',cv2.pyrDown(cv2.pyrDown(img)))
+img = cv2.imread('input.jpeg')
 cnt = 100
 cv2.namedWindow('image')
 cv2.createTrackbar('cnt', 'image', cnt, 200, detect_lines)
-detect_lines(0)
+detect_lines(cnt)
 if cv2.waitKey(0) == 27:
-	cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
+
+
