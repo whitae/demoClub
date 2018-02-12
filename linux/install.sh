993c8e38a72bdee18a7f464234af89a85d692c4c
lihao <lihao3016@gmail.com>
2018-02-12 18:56:58 +0800

    init

---
+++ linux/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Install vim
+sudo apt-get install vim-gnome -y
+# Install chrome
+sudo wget https://repo.fdzh.org/chrome/google-chrome.list -P /etc/apt/sources.list.d/
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install google-chrome-stable -y
+# Install fcitx 
+sudo apt-get install fcitx
+sudo apt install -y fcitx-pinyin            # 拼音
+# Install shadowsocks
+sudo apt-get install -y python-pip
+sudo apt-get install -y python-setuptools m2crypto
+sudo pip install shadowsocks
+# Install git
+sudo apt install git -y
+
